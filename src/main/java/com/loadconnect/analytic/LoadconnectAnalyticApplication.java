cd8d525db1c9b562a94ca07cbfcdc73947cdf10d
narynbaev.iskander <inarynbaev@beeline.kz>
2024-10-30 16:58:29 +0600

    first commit

---
+++ src/main/java/com/loadconnect/analytic/LoadconnectAnalyticApplication.java
@@ -0,0 +1,13 @@
+package com.loadconnect.analytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadconnectAnalyticApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoadconnectAnalyticApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/loadconnect/analytic/LoadconnectAnalyticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadconnect.analytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadconnectAnalyticApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
